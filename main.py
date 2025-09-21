43724b3647cf318617d8d59893d315d9512f2b87
bobojonabdurahmonov <pubmbobojon@gmail.com>
2025-09-21 20:45:46 +0500

    First Lesson

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api/register/{username}/{email}/{password}")
-async def root(username,email,password):
-    user = f"Username: {username}. Email: {email}. Password: {password}"
-    return {"status":user}
+@app.get("/api")
+async def root():
+    return {"status":"success"}
